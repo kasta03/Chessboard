7ace84969adc6820eb7cb6ea0e80eb5cec6b5327
Karol Starzyk <kastarzyk03@gmail.com>
2025-01-04 01:41:58 +0100

    implementing pieces printing on board

--- PROJECT/Project.cpp
+++ PROJECT/Project.cpp
@@ -14,88 +14,6 @@ int main()
 
     U64 mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
     printPiecePosition(mask);
-
-    // Move 1: White plays c4
-    moveWhitePawn(board, C2, C4);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 1: Black plays e6
-    moveBlackPawn(board, E7, E6);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 2: White plays Nf3
-    moveWhiteKnight(board, G1, F3);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 2: Black plays d5
-    moveBlackPawn(board, D7, D5);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 3: White plays d4
-    moveWhitePawn(board, D2, D4);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 3: Black plays Nf6
-    moveBlackKnight(board, G8, F6);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 4: White plays Nc3
-    moveWhiteKnight(board, B1, C3);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 4: Black plays Be7
-    moveBlackBishop(board, F8, E7);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 5: White plays Bg5
-    moveWhiteBishop(board, C1, G5);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Black castles queenside
-    moveBlackKing(board, E8, G8);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    std::cout << "-------------------------------------------------------------------" << std::endl;
-
-    // Move 6: White plays e3
-    moveWhitePawn(board, E2, E3);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 6: Black plays h6
-    moveBlackPawn(board, H7, H6);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 7: White plays Bh4
-    moveWhiteBishop(board, G5, H4);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 7: Black plays b6
-    moveBlackPawn(board, B7, B6);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 8: White plays cxd5
-    moveWhitePawn(board, C4, D5);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
-
-    // Move 8: Black plays Nxd5
-    moveBlackKnight(board, F6, D5);
-    mask = NS_mask::blackPiecesMask(board) | NS_mask::whitePiecesMask(board);
-    printPiecePosition(mask);
     
     drawBoard(window, board);
     
@@ -107,10 +25,7 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        
-        // Clear with dark gray background
-        window.clear(sf::Color(50, 50, 50));
-        
+                
         // Draw chess board
         drawBoard(window, board);
         
--- PROJECT/src/Graphic.cpp
+++ PROJECT/src/Graphic.cpp
@@ -26,8 +26,8 @@ void printPiecePosition(const U64 &picesType)
     {
         for (int file = 0; file < 8; ++file)
         {
-            U64 positionMask = 1ULL;
-            if ((positionMask <<= (file + rank * 8)) & picesType)
+            U64 positionMask = 1ULL << file + rank * 8;
+            if (positionMask  & picesType)
             {
                 std::cout << "1 ";
             }
@@ -52,7 +52,7 @@ void drawBoard(sf::RenderWindow &window, Board &board)
         {
             sf::RectangleShape square(sf::Vector2f(SQUARE_SIZE, SQUARE_SIZE));
             square.setPosition(file * SQUARE_SIZE, rank * SQUARE_SIZE);
-            if ((rank + file ) % 2 == 0)
+            if ((rank + file) % 2 == 0)
             {
                 square.setFillColor(sf::Color::White);
             }
@@ -63,5 +63,49 @@ void drawBoard(sf::RenderWindow &window, Board &board)
             window.draw(square);
         }
         std::cout << std::endl;
+    }
+    std::string path = PROJECTPATH;
+    std::vector<sf::Texture> textures(12);
+    textures[0].loadFromFile(path + "/img/WhitePawn.png");
+    textures[1].loadFromFile(path + "/img/WhiteKnight.png");
+    textures[2].loadFromFile(path + "/img/WhiteBishop.png");
+    textures[3].loadFromFile(path + "/img/WhiteRook.png");
+    textures[4].loadFromFile(path + "/img/WhiteQueen.png");
+    textures[5].loadFromFile(path + "/img/WhiteKing.png");
+    textures[6].loadFromFile(path + "/img/BlackPawn.png");
+    textures[7].loadFromFile(path + "/img/BlackKnight.png");
+    textures[8].loadFromFile(path + "/img/BlackBishop.png");
+    textures[9].loadFromFile(path + "/img/BlackRook.png");
+    textures[10].loadFromFile(path + "/img/BlackQueen.png");
+    textures[11].loadFromFile(path + "/img/BlackKing.png");
+
+    std::vector<sf::Sprite> sprites(12);
+    for (int i = 0; i < 12; ++i)
+    {
+        sprites[i].setTexture(textures[i]);
+        double scaleX = (double)SQUARE_SIZE / textures[i].getSize().x;
+        double scaleY = (double)SQUARE_SIZE / textures[i].getSize().y;
+        sprites[i].setScale(scaleX, scaleY);
+    }
+
+    for (int pieceType = 0; pieceType < 6; ++pieceType)
+    {
+        for (int pos = 0; pos < 64; pos++)
+        {
+            U64 mask = 1ULL << pos;
+            int rank = 7 - (pos / 8); // Zmiana: odwrócenie rzędu
+            int file = pos % 8;
+            if (mask & board._whitePieces[pieceType])
+            {
+                sprites[pieceType].setPosition(file * SQUARE_SIZE, rank * SQUARE_SIZE);
+                window.draw(sprites[pieceType]);
+                std::cout << "White piece: " << pieceType << " at position: " << pos << std::endl;
+            }
+            if (mask & board._blackPieces[pieceType])
+            {
+                sprites[pieceType + 6].setPosition(file * SQUARE_SIZE, rank * SQUARE_SIZE);
+                window.draw(sprites[pieceType + 6]);
+            }
+        }
     }
 }
